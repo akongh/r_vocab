c00c641cebdf59a02a004eb693e4f44135d782ae
Andrei Korzhyts <contactandreikorzhyts@gmail.com>
2023-09-09 20:09:47 +0300

    rational_vocabulary

--- rvocab.py
+++ rvocab.py
@@ -4,14 +4,14 @@ file = open("text.txt")
 text = file.read().lower()
 file.close()
 
-text = re.sub("[\n\r\t]", " ", text)
+text = re.sub("[\\W\\d_]", " ", text)
 text = re.sub(" {2,}", " ", text)
-text = re.sub("[.,?!] ", " ", text)
 text = text.strip()
 
 text = text.split(" ")
+clear_text = []
 
 for word in text:
-    print(word)
-
-# print(text)
+    if len(word) > 1:
+        clear_text.append(word)
+        print(word)

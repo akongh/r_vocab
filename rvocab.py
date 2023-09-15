310bed3889c33b361c6ce860b21f77f40e6d1985
Andrei Korzhyts <contactandreikorzhyts@gmail.com>
2023-09-16 01:35:11 +0300

    rational_vocabulary

--- rvocab.py
+++ rvocab.py
@@ -1,4 +1,9 @@
 import re
+import os
+
+if not os.path.exists('text.txt'):
+    print("ОШИБКА: text.txt не найден.")
+    exit()
 
 file = open("text.txt")
 words = file.read().lower()
@@ -36,8 +41,17 @@ resulting_list = []
 for word in resulting_words:
     resulting_list.append(str(word[0]) + "  " + word[1])
 
+about_unique_words = "Уникальных слов: " + str(len(resulting_words))
+
 rational_vocabularu = "\n".join(resulting_list)
-rational_vocabularu = "Уникальных слов: " + str(len(resulting_words)) + "\n\n" + rational_vocabularu
+rational_vocabularu = about_unique_words + "\n\n" + rational_vocabularu
 
 file = open("rational_vocabularu.txt", "w")
 file.write(rational_vocabularu)
+
+if os.path.exists('rational_vocabularu.txt'):
+    print("СДЕЛАНО: rational_vocabularu.txt успешно создан.")
+    print(about_unique_words)
+else:
+    print("ОШИБКА: rational_vocabularu.txt не был создан.")
+    exit()

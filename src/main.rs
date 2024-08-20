7ea13b2e02bbffdf7fa4c90e99eae17b79ee92b0
Andrei Korzhyts <contactandreikorzhyts@gmail.com>
2024-08-21 01:41:23 +0300

    r_vocab

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("src/text.txt").expect("<!> Can't open file!");
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)
+        .expect("<!> Can't read the file!");
+
+    let mut file = File::create("src/r_vocab.txt").expect("<!> Can't create file!");
+
+    file.write_all(content.as_bytes())
+        .expect("<!> Can't write file!");
 }

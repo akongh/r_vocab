584118d0a398fb53d6bc38d9db4bbdfccff825a7
Andrei Korzhyts <contactandreikorzhyts@gmail.com>
2024-08-23 20:10:18 +0300

    r_vocab

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::prelude::*;
 fn main() {
     let mut file = File::open("src/text.txt").expect("<!> Can't open file!");
-    let mut content = String::new();
+    let mut raw_vocab = String::new();
 
-    file.read_to_string(&mut content)
+    file.read_to_string(&mut raw_vocab)
         .expect("<!> Can't read the file!");
 
     let re1 = Regex::new(r"[^A-Za-z]").unwrap();
@@ -13,10 +13,10 @@ fn main() {
 
     let mut file = File::create("src/r_vocab.txt").expect("<!> Can't create file!");
 
-    content = re1.replace_all(&mut content, " ").to_string();
-    content = re2.replace_all(&mut content, " ").trim().to_string();
+    raw_vocab = re1.replace_all(&mut raw_vocab, " ").to_string();
+    raw_vocab = re2.replace_all(&mut raw_vocab, " ").trim().to_string();
 
-    let result4: Vec<String> = content
+    let result4: Vec<String> = raw_vocab
         .split(" ")
         .map(|s| s.to_string().to_lowercase())
         .collect();
